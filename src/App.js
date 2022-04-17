fd1a53f6ef57eb2578ebec487b0a7031d1a2e2df
zackzellto <39507770+zackzellto@users.noreply.github.com>
2022-04-16 21:24:10 -0600

    added footer links and page sections

--- src/App.js
+++ src/App.js
@@ -1,19 +1,89 @@
 import "./App.css";
-
+import GitHubLogo from "./media/github-logo.png";
+import LinkedInLogo from "./media/linkedin-logo.png";
+import EmailLogo from "./media/email-logo.png";
 import ResponsiveNavbar from "./Navbar.jsx";
 import Container from "react-bootstrap/Container";
+import EmailModal from "./components/email-modal";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Container>
-          <div>
-            <ResponsiveNavbar />
-          </div>
-        </Container>
-      </header>
-    </div>
+    <>
+      <ResponsiveNavbar />
+      <div className="App">
+        <header className="App-header">
+          <Container>
+            <div>
+              <h1>Hi. </h1>
+              <h1>I'm Zack.</h1>
+              <h2>a full-stack software developer.</h2>
+            </div>
+            <div>
+              <h3>Let's get to know eachother!</h3>
+            </div>
+          </Container>
+        </header>
+        <main>
+          <Container>
+            <hr />
+            <div className="about">
+              <h2>ABOUT ME</h2>
+              <br />
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+            <hr />
+            <div className="portfolio">
+              PORTFOLIO
+              <br />
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+            <hr />
+          </Container>
+        </main>
+        <footer>
+          <Container className="contact-container">
+            <div>
+              <a
+                target="_blank"
+                href="https://github.com/zackzellto"
+                rel="noreferrer"
+              >
+                <img
+                  className="github-link"
+                  src={GitHubLogo}
+                  alt="github-logo"
+                />
+              </a>
+
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/zackzellto"
+                rel="noreferrer"
+              >
+                <img
+                  className="linkedin-link"
+                  src={LinkedInLogo}
+                  alt="linkedin-logo"
+                />
+              </a>
+              <EmailModal className="email-link" />
+            </div>
+          </Container>
+        </footer>
+      </div>
+    </>
   );
 }
 
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -10,10 +10,10 @@ function ResponsiveNavbar() {
     <Navbar.Brand href="#home">Zack Zellto</Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="nav-links mr-auto">
+    <Nav className="nav-links ms-auto">
       <Nav.Link className='about-me-link' href="#about-me">About Me</Nav.Link>
       <Nav.Link className='portfolio-link'href="#portfolio">Portfolio</Nav.Link>
-      <Nav.Link className='resume-link' href="#resume">Resume</Nav.Link>
+      <Nav.Link className='resume-link' href="https://drive.google.com/uc?export=download&id=1fpOclex1j8RmznkbDY9x10z8hfG6SOp8">Resume</Nav.Link>
       <Nav.Link className='contact-link' href="#contact">Contact</Nav.Link>
     </Nav>
     </Navbar.Collapse>
