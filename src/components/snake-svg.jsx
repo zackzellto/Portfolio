3e20a48a8ba479ceda63f51bee969d61a8a722ba
zackzellto <39507770+zackzellto@users.noreply.github.com>
2022-04-19 23:49:46 -0600

    Got the transition working for project card. Need to overlay logo next

--- src/components/snake-svg.jsx
+++ src/components/snake-svg.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Image } from 'react-bootstrap';
+import { Image, Container } from 'react-bootstrap';
+import SnakeLogo from '../media/snake-thumbnail.png';
 
 
 
@@ -8,18 +9,27 @@ export default function SnakeGameSVG() {
 const SnakeGameGif = require('../media/snakedemo.gif');
 
   return (
-    <>
-    <a href='https://zackzellto.github.io/snake-game/' target="_blank" rel='noreferrer'>
+    <Container>
+      
     <svg id='projectSVG' viewBox="0 0 1920 1080">
+    <a id='projectLink' href='https://zackzellto.github.io/snake-game/' target="_blank" rel='noreferrer'>
+    
     <foreignObject x="0" y="0" width="100%" height="100%">
-        <Image src={SnakeGameGif} alt='snakegif'></Image>
+        <Image id='projectGif' src={SnakeGameGif} alt='snakegif'></Image>
          </foreignObject>
-         <rect id='projectRect' x="0" y="0" width="100%" height="100%" fill="#141e30" />
+         <rect id='projectRect' x="0" y="0" width="100%" height="100%"></rect>
 
-         <text id='projectText' fill='white' x = '20%' y = '60%'> Snake Game </text>
-</svg>
-</a>
+      <defs>
+        <linearGradient id="projectGradient">
+          <stop offset="0%" stopColor="rgb(58, 60, 74)" />
+          <stop offset="100%" stopColor="rgb(36, 38, 50)" />
+        </linearGradient>
+      </defs>
+      
+      </a>
+      </svg><Image id='projectImage' src={SnakeLogo} width={100} height={100} />
+      </Container>
 
-    </>
+    
   );  
 }
